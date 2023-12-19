cb99dfc86d893def2b3da7752bd7227c28495cd0
Ramakrishna Kella <ramakrishna.kella@moschip.com>
2023-12-19 20:55:08 +0530

    added 2nd script variables

---
+++ 02-variables.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+echo "Ramesh:: Hello Suresh, Good Morning"
+echo "Suresh:: Hi Ramesh, Very Good Morning"
+echo "Ramesh:: How are you Suresh!!"
+echo "Suresh:: I am good, How are you Ramesh"
