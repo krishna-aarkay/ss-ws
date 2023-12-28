618e0333fb9464b966bbc3c20c15b0d12ffad526
Ramakrishna Kella <ramakrishna.kella@moschip.com>
2023-12-28 22:25:40 +0530

    updates

--- 08-conditions.sh
+++ 08-conditions.sh
@@ -3,8 +3,9 @@ n=$1
 if [ $n -gt 100 ]
 then
     echo "Yes, given munber "$n" is greater than 100"
-else if [ $n -e 100 ]
+elif [ $n -eq 100 ]
+then	
     echo "No, given numbr "$n" is equal to 100 "
 else
-    echo "given number id leass than 100"
+    echo "given number "$n" is leass than 100"
 fi
---
+++ 09-install-mysql.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+ID=$(id -u)
+if [ $ID -ne 0 ]
+then
+    echo "Error:: Please install with root account"
+    exit 1
+else
+    echo "You are ROOT user"
+fi
+
+yum install mysql -y
+if [ $? -ne 0 ]
+then
+    echo "Installation faild"
+    exit 1
+else
+    echo "Installation successful"
