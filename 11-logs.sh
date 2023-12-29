64930b81cd30aeef8cc773f03cd3142e72f83344
Ramakrishna Kella <ramakrishna.kella@moschip.com>
2023-12-29 22:22:28 +0530

    updates

--- 11-logs.sh
+++ 11-logs.sh
@@ -4,26 +4,26 @@ ID=$(id -u)
 echo "Sript name:: $0"
 TIMESTAMP=$(date +%F-%H-%M-%S)
 LOGFILE=/tmp/$0-$TIMESTAMP.log
-R="\e[31m"
-G="\e[32m"
-N="\e[0m"
+R='\e[31m'
+G='\e[32m'
+N='\e[0m'
 
 VALIDATE(){
     if [ $1 -ne 0 ]
     then
-        echo "$2 $R Installation faild $N"
+        echo -e "$2 $R Installation faild $N"
         exit 1
     else
-        echo "$2 $G Installation successful $N"
+        echo -e "$2 $G Installation successful $N"
     fi
 }
 
 if [ $ID -ne 0 ]
 then
-    echo "Error:: Please install with root account"
+    echo -e " $R Error:: Please install with root account $N"
     exit 1
 else
-    echo "You are ROOT user"
+    echo -e "$G You are ROOT user $N"
 fi
 
 yum install mysql -y &>> $LOGFILE
