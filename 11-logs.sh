ebb32698f036daef0215618d601b09461067b285
Ramakrishna Kella <ramakrishna.kella@moschip.com>
2023-12-29 22:08:49 +0530

    updates

--- 11-logs.sh
+++ 11-logs.sh
@@ -4,14 +4,17 @@ ID=$(id -u)
 echo "Sript name:: $0"
 TIMESTAMP=$(date +%F-%H-%M-%S)
 LOGFILE=/tmp/$0-$TIMESTAMP.log
+R="\e[31m"
+G="\e[32m"
+N="\e[0m"
 
 VALIDATE(){
     if [ $1 -ne 0 ]
     then
-        echo '$2 "\e[31m" Installation faild "\e[0m"'
+        echo "$2 $R Installation faild $N"
         exit 1
     else
-        echo "$2 \e[32m Installation successful \e[0m"
+        echo "$2 $G Installation successful $N"
     fi
 }
 
