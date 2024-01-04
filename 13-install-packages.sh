262511641ec7e6855a2db7d09b6a50b05c64fd3c
Ramakrishna Kella <ramakrishna.kella@moschip.com>
2024-01-04 18:26:51 +0530

    updates

--- 13-install-packages.sh
+++ 13-install-packages.sh
@@ -2,7 +2,17 @@
 ID=$(id -u)
 R="\e[31m"
 G="\e[32m"
+Y="\e[33m"
 N="\e[0m"
+VALIDATE(){
+    if [ if $1 -ne 0 ]
+    then
+        echo -e " $2 ... $R FAILED $N"
+    else
+        echo -e " $2 ... $G SUCCESS $N"
+    fi
+}
+
 if [ $ID -ne 0 ]
 then
     echo -e "$R ERROR:: Please install the packages using root only $N"
@@ -13,4 +23,19 @@ fi
 
 echo -e "ALL arguments passed::$G $@ $N"
 echo -e "Total arguments passed::$G $# $N"
+
+for package in $@
+do
+    yum list installed $package
+    if [ $? -ne 0 ]
+    then
+        yum install $package -y
+        VALIDATE $? "Installation of $package"
+    else
+        echo "$pacjage is already installed ... $Y SKIPPING $N"
+    fi
+
+
+done
+
 
