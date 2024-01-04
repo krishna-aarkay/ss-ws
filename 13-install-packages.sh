33a455b40d224858d332298f80fc5d38d98b6386
Ramakrishna Kella <ramakrishna.kella@moschip.com>
2024-01-04 18:12:59 +0530

    updates

--- 13-install-packages.sh
+++ 13-install-packages.sh
@@ -11,6 +11,6 @@ else
     echo -e "$G Hola:: You are root user $N"
 fi
 
-echo "ALL arguments passed::$G $@ $N"
-echo "Total arguments passed::$G $# $N"
+echo -e "ALL arguments passed::$G $@ $N"
+echo -e "Total arguments passed::$G $# $N"
 
