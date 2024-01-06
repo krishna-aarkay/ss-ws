4b27e6eb8e6bb585c61606bc492743a03f42811a
Ramakrishna Kella <ramakrishna.kella@moschip.com>
2024-01-06 22:01:58 +0530

    shell-script

--- 09-install-mysql.sh
+++ 09-install-mysql.sh
@@ -1,16 +1,5 @@
 #!/bin/bash
 ID=$(id -u)
-
-VALIDATE(){
-    if [ $? -ne 0 ]
-    then
-        echo "Installation faild"
-        exit 1
-    else
-        echo "Installation successful"
-    fi
-}
-
 if [ $ID -ne 0 ]
 then
     echo "Error:: Please install with root account"
@@ -20,5 +9,10 @@ else
 fi
 
 yum install mysql -y
-
-VALIDATE
+if [ $? -ne 0 ]
+then
+    echo "Installation faild"
+    exit 1
+else
+    echo "Installation successful"
+fi
---
+++ 15.delete-old-logs.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+SOURCE_DIR=/tmp/shell-script-logs
+
+if[ ! -d $SOURCE_DIR ]
+then
+    echo " $SOURCE_DIR not exist "
+fi
+
+FILES_TO_DELETE=$(find $SOURCE_DIR -type f -mtime +14 -name "*.log")
+
+while IFS= read -r line
+do
+    echo "Deleting file: $line"
+
+done <<< $FILES_TO_DELETE
